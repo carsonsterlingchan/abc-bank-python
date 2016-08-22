ed3579696134902c1b0185fce6fee09216f1ba5f
carsonsterlingchan <carson@carsonsterlingchan.com>
2016-08-21 23:42:51 -0400

    v0.0.1
    now method is modified to return UTC current date and time.
    
    customedDate method is added to allow user to provide a backdated date
    if necessary.

--- abcbank/date_provider.py
+++ abcbank/date_provider.py
@@ -1,7 +1,22 @@
+"""
+Class: DateProvider
+
+Note:
+
+now method is modified to return UTC current date and time.
+customedDate method is added to allow user to provide a backdated date if necessary. 
+
+
+"""
 from datetime import datetime
 
 
 class DateProvider:
     @staticmethod
     def now():
-        return datetime.now()
+        # Make sure all the dates are in UTC timezone
+        return datetime.utcnow()
+
+    @staticmethod
+    def customedDate(year, month, day):
+        return datetime(year, month, day)
