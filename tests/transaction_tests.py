09fbe85160fde52082b472c81b56960fba8bdeb1
carsonsterlingchan <carson@carsonsterlingchan.com>
2016-08-21 23:53:19 -0400

    v0.01
    Added a transaction creation with customed date

--- tests/transaction_tests.py
+++ tests/transaction_tests.py
@@ -1,8 +1,13 @@
-from nose.tools import assert_is_instance
+from nose.tools import assert_is_instance, assert_equal
 
-from transaction import Transaction
-
+from abcbank.transaction import Transaction
+from abcbank.date_provider import DateProvider
+from datetime import datetime
 
 def test_type():
     t = Transaction(5)
     assert_is_instance(t, Transaction, "correct type")
+
+def test_transDate():
+    t = Transaction(10, DateProvider().customedDate(2016, 8, 20))
+    assert_equal(t.transactionDate, datetime(2016, 8, 20))
